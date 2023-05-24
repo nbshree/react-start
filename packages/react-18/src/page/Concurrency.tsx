ccf8b9b2a44f2e3d7862ed3bd64de166b4ccff07
nbshree <401163814@qq.com>
2023-05-24 21:43:04 +0800

    setInterval

--- packages/react-18/src/page/Concurrency.tsx
+++ packages/react-18/src/page/Concurrency.tsx
@@ -1,19 +1,11 @@
-import React, {
-  useState,
-  useEffect,
-  useDeferredValue,
-  useRef,
-} from "react";
-
+import React, { useState, useEffect, useDeferredValue, useRef } from "react";
 
 const ShowValue = (props: any) => {
   console.log("props.list", props.list);
   return (
     <div>
       {props.list.map((_: any, i: number) => (
-        <div key={i}>
-          {i}
-        </div>
+        <div key={i}>{i}</div>
       ))}
     </div>
   );
@@ -23,9 +15,15 @@ const ShowValueMemo = React.memo(ShowValue);
 const Concurrency: React.FC = () => {
   const [list, setList] = useState<any[]>([]);
   const [showValue, setShowValue] = useState<number>(0);
+  const showValueRef = useRef(0);
   const ref = useRef<any>(null);
   useEffect(() => {
     setList(new Array(20000).fill(null));
+    // setInterval(() => {
+    //   if (showValueRef.current < 20) {
+    //     ref.current.click();
+    //   }
+    // }, 100);
     setTimeout(() => {
       ref.current.click();
     }, 200);
@@ -40,7 +38,8 @@ const Concurrency: React.FC = () => {
         style={{ cursor: "pointer" }}
         onClick={() => {
           console.log("1", 1);
-          setShowValue(2222);
+          showValueRef.current += 1;
+          setShowValue((pre) => pre + 1);
         }}
       >
         点我{showValue}
